aaac2dc4903820c8b0c1fe0d97cf67ec0a0b4470
Andrey <Andrey@DESKTOP-NRKMLIG>
2023-02-12 01:44:13 +0300

    Create BusinessLogicRegister

---
+++ Meetup/Meetup.BLL/BusinessLogicRegister.cs
@@ -0,0 +1,17 @@
+﻿using Meetup.DAL.DI;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Meetup.BLL;
+
+public static class BusinessLogicRegister
+{
+    public static void AddMeetupBll(
+        this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        //services.AddScoped<IEventService<Event, int>, EventService>();
+        //services.AddScoped<ISpeakerService<Speaker, int>, SpeakerService>();
+        services.AddDataContext(configuration);
+    }
+}
