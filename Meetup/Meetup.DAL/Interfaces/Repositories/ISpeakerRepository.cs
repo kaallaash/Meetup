54de83ada8938f1b87b0646c67c155c6ec7deb26
Andrey <Andrey@DESKTOP-NRKMLIG>
2023-02-12 00:07:34 +0300

    add repositories

---
+++ Meetup/Meetup.DAL/Interfaces/Repositories/ISpeakerRepository.cs
@@ -0,0 +1,8 @@
+﻿using Meetup.Core.Repositories;
+
+namespace Meetup.DAL.Interfaces.Repositories;
+
+public interface ISpeakerRepository<T1> : IBaseCrudRepository<T1>
+{
+    Task<T1?> GetByEmail(string email, CancellationToken cancellationToken);
+}
