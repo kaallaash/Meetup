8f70ac417a9586fb392ec2e47d5a3ee3af8fa2a1
Andrey <Andrey@DESKTOP-NRKMLIG>
2023-02-11 22:13:19 +0300

    create Core lib

---
+++ Meetup/Core/Meetup.Core/Repositories/IBaseCrudRepository.cs
@@ -0,0 +1,11 @@
+﻿using Meetup.Core.Interfaces.CrudOperations;
+
+namespace Meetup.Core.Repositories;
+
+public interface IBaseCrudRepository<T> :
+    IGetByIdOperation<T>,
+    IGetAllOperation<T>,
+    ICreateOperation<T>,
+    IUpdateOperation<T>,
+    IDeleteOperation<T>
+{ }
