5efe8a054bb8b145f787a16fb04fd20cc6ae07ab
Andrey <Andrey@DESKTOP-NRKMLIG>
2023-02-13 15:39:40 +0300

    add IBaseCrudService

---
+++ Meetup/Core/Meetup.Core/Services/IBaseCrudService.cs
@@ -0,0 +1,11 @@
+﻿using Meetup.Core.Interfaces.CrudOperations;
+
+namespace Meetup.Core.Services;
+
+public interface IBaseCrudService<T1, in T2> :
+    IGetByIdOperation<T1>,
+    IGetAllOperation<T1>,
+    ICreateOperation<T1>,
+    IUpdateOperation<T1>,
+    IDeleteOperation<T2>
+{ }
